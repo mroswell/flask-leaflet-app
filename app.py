f5d2bed9e4c8c25ab375fa52534f228584bfa02c
Marjorie Roswell <mroswell@gmail.com>
2014-08-06 00:46:00 -0400

    add map to index page

--- app.py
+++ app.py
@@ -6,11 +6,21 @@ app = Flask(__name__)
 csv_path = './static/la-riots-deaths.csv'
 csv_obj = csv.DictReader(open(csv_path, 'r'))
 csv_list = list(csv_obj)
+# http://stackoverflow.com/questions/1747817/python-create-a-dictionary-with-list-comprehension
+csv_dict = dict([[o['id'], o] for o in csv_list])
 
 @app.route("/")
 def index():
     return render_template('index.html', 
-           object_list=csv_list,)
+           object_list=csv_list,
+)
+
+
+@app.route('/<number>/')
+def detail(number):
+    return render_template('detail.html',
+        object=csv_dict[number],
+    )
 
 if __name__ == '__main__':
     app.run(
